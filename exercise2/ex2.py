35aeb3a0b118b9d0c9b0c758f5b7f1c6cc25d8c9
mariannagaggiano-cloud <marianna.gaggiano@gmail.com>
2025-11-07 19:42:15 +0000

    commit

--- exercise2/ex2.py
+++ exercise2/ex2.py
@@ -1,6 +1,6 @@
 #3 Methods Monte Carlo Integration
 #Main function is f = exp(-x^2)
-# Control variate is g = exp(-x)
+# Control variate is g = exp(-x) 
 # Calculate If, sigmaIf, If' and sigmaIf'
 # If=0.746824132812427
 
@@ -11,7 +11,7 @@ print("Calculating f = exp(-x^2) integral from 0 to 1 with three different MC me
 print("Sampling mean, Importance Sampling and Control variate")
 print()
 
-number = 10  # number of random number try 10, 100, 1000, 10000
+number_array = [10, 100, 1000, 10000]  # number of random number try 10, 100, 1000, 10000
 #factor = 1.0
 trueI = 0.746824132812427
 Ig_value = 1-np.exp(-1)
@@ -22,16 +22,24 @@ print()
 rn = np.random.rand(number)
 
 
-# calculate integrals (simple averages in this case)
+# calculate integrals
+
 #sampling mean
 f = np.exp(-rn**2)
 I_SM = np.sum(f)/number
 
+var_SM = (number/(number-1))*((np.sum(f**2)/number) - ((np.sum(f)/number)**2))
+
+
 #importance sampling
 y = rn
-Ginv_y = - np.log(1-y*(1-np.exp(-1))) 
-f_Ginv_y = (1 - np.exp(-1)) * np.exp(-Ginv_y**2)/np.exp(-Ginv_y)
-I_IS = np.sum(f_Ginv_y)/number
+Ginv_y = - np.log(1-y*(1-np.exp(-1)))           #G^-1(y)
+f_Ginv_y = np.exp(-Ginv_y**2)                   #f(G^-1(y))
+g_Ginv_y = np.exp(-Ginv_y)/(1 - np.exp(-1))     #g(G^-1(y)) in this case g has to be normalized
+I_IS = np.sum(f_Ginv_y/g_Ginv_y)/number
+
+var_IS = (number/(number-1))*((np.sum((f_Ginv_y/g_Ginv_y)**2)/number) - (np.sum((f_Ginv_y/g_Ginv_y)/number)**2))
+
 
 #control variates
 g = np.exp(-rn)
@@ -39,17 +47,13 @@ f_prime= f-g
 I_g = np.sum(g)/number
 I_CV = np.sum(f_prime)/number + Ig_value
 
-# calculate variances
-var_f = (number/(number-1))*((np.sum(f**2)/number) - ((np.sum(f)/number)**2))
 var_g = (number/(number-1))*((np.sum(g**2)/number) - ((np.sum(g)/number)**2))
 cov_fg = (1/(number-1))*np.sum((f-I_SM)*(g-I_g))
-var_fprime = (number/(number-1))*((np.sum(f_prime**2)/number) - ((np.sum(f_prime)/number)**2))
-var_f_Ginv_y=(number/(number-1))*((np.sum(f_Ginv_y**2)/number) - ((np.sum(f_Ginv_y)/number)**2))
+var_CV = (number/(number-1))*((np.sum(f_prime**2)/number) - ((np.sum(f_prime)/number)**2))
 
-print()
-print("*******************************************************************************************")
+#print results
+
 print("***************************>>> PRINT RESULTS <<<*******************************************")
-print("*******************************************************************************************")
 print()
 print("If(samp meam) = ", I_SM, " with SEM = ", np.sqrt(var_f/number), "(", 100*np.sqrt(var_f/number)/I_SM, "%)")
 print("If - true % = ", 100*(I_SM - trueI)/trueI)
@@ -71,5 +75,3 @@ print("CoVar = ", cov_fg)
 print("corr factor = ", cov_fg/np.sqrt(var_f*var_g))
 print("optimal M = ", cov_fg/var_g)
 print("************************************>>> END <<<********************************************")
-print("*******************************************************************************************")
-
